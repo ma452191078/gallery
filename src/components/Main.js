e02e7e4f5a1ff468b4dfe0469861cd09dc8da3f0
Code <majingyuanno.1@gmail.com>
2017-05-20 08:20:15 +0800

    页面

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,11 @@
 require('normalize.css/normalize.css');
-require('styles/App.scss');
+require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
 //获取图片资源
-var imageDatas = require('../data/imageData.json');
+let imageDatas = [];
+imageDatas = require('json-loader!../data/imageData.json');
 //转换为图片URL
 imageDatas = (function genImageURL(imageDatasArr) {
   for (var i = 0; i < imageDatasArr.length; i++){
@@ -15,15 +15,40 @@ imageDatas = (function genImageURL(imageDatasArr) {
   }
 })(imageDatas);
 
+// 创建图片组件
+var ImgFigure = React.createClass({
+  render: function () {
+    return (
+      <figure>
+        <img src={this.props.data.imageURL}
+              alt={this.props.data.title}/>
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
+
 class AppComponent extends React.Component {
   render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+    console.log(imageDatas);
+    imageDatas.forEach(function(value) {
+
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+
     return (
       <section className = "stage">
         <section className="img-sec">
 
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-
+          {controllerUnits}
         </nav>
       </section>
     );
